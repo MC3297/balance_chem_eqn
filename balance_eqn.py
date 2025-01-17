5c4b1d985f2964ee5486fe745c7e7cc5083b0be3
Matthew Chen <97566000+MC3297@users.noreply.github.com>
2025-01-17 04:52:27 -0500

    Add files via upload

---
+++ balance_eqn.py
@@ -0,0 +1,37 @@
+eqn = "_H3PO4 + _KOH -> _K3PO4 + _H2O"
+
+# do some string parsing
+eqn = eqn.replace(" ", "")
+lhs, rhs = eqn.split("->")
+print(eqn)
+print(lhs,rhs)
+
+lhs_terms = lhs.split("+")
+rhs_terms = rhs.split("+")
+print(lhs_terms, rhs_terms)
+
+# convert string (can be empty string) to int
+def to_int(s):
+    if len(s) == 0: return 1
+    else: return int(s)
+
+# takes a molecule and split it into each atom's frequencies
+# ex: H2O -> {'H':2, 'O':1}
+def molecules_freq(str):
+    ans = {}
+    atom = ""
+    num = ""
+    for c in str:
+        if c.islower():
+            atom += c
+        if c.isupper():
+            ans[atom] = to_int(num)
+            atom = c
+            num = ""
+        if c.isnumeric():
+            num += c
+    ans[atom] = to_int(num)
+    ans.pop("")
+    return ans
+
+print(molecules_freq("H2O"))
